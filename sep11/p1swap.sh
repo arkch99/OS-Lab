67aaf28b48457ab5eee3b507d66a836d88cbd42b
arkch99 <arko1999@gmail.com>
2020-09-11 13:52:34 +0530

    Include swap using third variable

--- sep11/p1swap.sh
+++ sep11/p1swap.sh
@@ -8,13 +8,28 @@ a=`expr $a + $b`
 b=`expr $a - $b`
 a=`expr $a - $b`
 
+echo "Without using a third variable: "
+
 echo "a is now " $a
 echo "b is now " $b
 
-#arko@arko:~/os_lab/sep11$ sh p1swap.sh 
+echo "Swapping them back with a third variable..."
+
+t=$a
+a=$b
+b=$t
+
+echo "a is now " $a
+echo "b is now " $b
+
+# arko@arko:~/os_lab/sep11$ sh p1swap.sh 
 # 2
 # 3
 # a is  2
 # b is  3
+# Without using a third variable: 
 # a is now  3
 # b is now  2
+# Swapping them back with a third variable...
+# a is now  2
+# b is now  3
